87445ef324712c3cabf545c29e50c49299c21a89
stefano <mishtir@gmail.com>
2023-08-14 10:17:19 +0200

    Donw task 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Asset } from '../shared/models/asset.model'
 import { AssetService } from '../shared/services/asset.service'
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,28 @@ import { AssetService } from '../shared/services/asset.service'
 })
 export class HomePage {
   assets: Asset[] = []
+  isToastOpen = false;
 
   constructor(private assetService: AssetService) {}
 
   ionViewWillEnter(): void {
     this.assets = []
-    this.assetService.getAll().subscribe(output => {
+    this.assetService.getAll().pipe(
+      catchError(_error => {
+        // console.log(error.message)
+        this.setToastOpen(true)
+        return [];
+      })
+    ).subscribe(output => {
       if(output.ok){
         this.assets = output.data
+      }else{
+        this.setToastOpen(true)
       }
     })
+  }
+
+  setToastOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
   }
 }
